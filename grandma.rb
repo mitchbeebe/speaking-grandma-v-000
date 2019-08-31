1722f291dc97f09c4d300d2a2bbb930f39f76106
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-08-31 15:09:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,11 @@ def speak_to_grandma(phrase)
   
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "NO, NOT SINCE 1938!"
+  end
   
 end
 # Whatever you say to grandma, she should respond with
