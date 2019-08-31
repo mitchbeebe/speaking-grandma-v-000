03dd15b7878bbc0e66995fd6cf90521b3307e3dd
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-08-31 15:08:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  
+  if phrase != phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase
+  
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
