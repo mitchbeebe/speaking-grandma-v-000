7ad4405d69df9c63141f2ae02da7ce900cbd1e76
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-08-31 15:10:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 def speak_to_grandma(phrase)
   
   if phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
   
 end
